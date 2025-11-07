bf49783af4f7617b3002cbfb1bac14e1aa034e11
Sergio Velandia <sergioorlandovq@gmail.com>
2025-11-07 12:33:49 -0500

    Interfaz de Rol

---
+++ RecyRoute/Repositories/Interfaces/IRolRepository.cs
@@ -0,0 +1,17 @@
+using RecyRoute.Modelos;
+
+namespace RecyRoute.Repositories.Interfaces
+{
+    public interface IRolRepository
+    {
+        Task<List<Rol>> ObtenerRoles();
+
+        Task<Rol> ObtenerRol(Guid idrol);
+
+        Task<Rol> CrearRol(Rol rol);
+
+        Task<Rol> ActualizarRol(Rol rol);
+
+        Task<bool> EliminarRol(Guid idrol);
+    }
+}
